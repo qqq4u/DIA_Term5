abf9d2aeb456a6cd62adc04484a3a128e3038a80
Данила Поляков <danila.polyakov@vk.team>
2024-01-09 16:01:02 +0300

    fixes for 6lab/hw

--- bmstu/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(CustomUser)
-admin.site.register(Parking)
-admin.site.register(Ticket)
--- bmstu/app/management/commands/clean_db.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from app.models import *
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        Ticket.objects.all().delete()
-        Parking.objects.all().delete()
